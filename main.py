6769a6215e6e3e3909c87298e5e3cbbaf83a095b
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-08-27 14:18:46 +0000

    init game player

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pygame
 pygame.init()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.max_health = 100
+        self.health = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load('assets/player.png')
+        self.rect = self.image.get_rect()
+
+#init game player
+player = Player()
+
 # init game screen
 pygame.display.set_caption("Comet fall Game")
 screen =  pygame.display.set_mode((1080, 720))
