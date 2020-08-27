f596702d1702d41e8c628a649d2e5acc16132027
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-08-27 17:37:18 +0000

    add bullets moves rotate animation

--- parts/bullet.py
+++ parts/bullet.py
@@ -11,9 +11,17 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 125
         self.rect.y = player.rect.y + 95
+        self.origin_image = self.image
+        self.angle = 0
+
+    def rotate(self):
+        self.angle += 12
+        self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
+        self.rect = self.image.get_rect(center= self.rect.center)
 
     def move(self):
         self.rect.x += self.velocity
+        self.rotate()
         # detect if bullet over the screen and destroy it
         if self.rect.x > 1080:
             self.remove()
