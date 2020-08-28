4b9fad03271712b24d5550416e0e36c1068ce821
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-08-28 04:09:47 +0000

    update monster damage logic and attack

--- parts/monster.py
+++ parts/monster.py
@@ -8,7 +8,7 @@ class Monster(pygame.sprite.Sprite):
         self.game = game
         self.max_health = 100
         self.health = 100
-        self.attack = 5
+        self.attack = 0.3
         self.velocity = random.randint(1, 3)
         self.image = pygame.image.load('assets/mummy.png')
         self.rect = self.image.get_rect();
@@ -17,11 +17,11 @@ class Monster(pygame.sprite.Sprite):
 
     def damage(self, amount):
         #if dead, reset the monster
-        if self.health <= 0:
+        if self.health - amount > 0: self.health -= amount
+        else:
             self.rect.x = 1000 + random.randint(1, 300)
             self.velocity = random.randint(1, 3)
             self.health = self.max_health
-        else: self.health -= amount
 
     def update_health_bar(self, surface):
         pygame.draw.rect(surface, (60, 63, 60), [self.rect.x + 10, self.rect.y - 20, self.max_health, 5])
