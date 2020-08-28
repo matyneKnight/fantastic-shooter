69ddf9a4be0e417ff1675e40de5479bd5acf1002
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-08-28 04:53:09 +0000

    add starting play button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from parts.game import Game
 
 pygame.init()
@@ -10,12 +11,19 @@ screen = pygame.display.set_mode((1080, 720))
 
 # load new game
 game = Game()
-# load game background image
+# load game background, starting banner, play button images
 background = pygame.image.load('assets/bg.jpg')
+
 banner = pygame.image.load('assets/banner.png')
 banner = pygame.transform.scale(banner, (500, 500))
 banner_rect = banner.get_rect()
-banner_rect.x = screen.get_width() / 4
+banner_rect.x = math.ceil(screen.get_width() / 4)
+
+play_button = pygame.image.load('assets/button.png')
+play_button = pygame.transform.scale(play_button, (400, 150))
+play_button_rect = play_button.get_rect()
+play_button_rect.x = math.ceil(screen.get_width() / 3.33)
+play_button_rect.y = math.ceil(screen.get_height() / 2)
 
 running = True
 
@@ -28,6 +36,7 @@ while running:
     if game.is_playing:
         game.update(screen)
     else:
+        screen.blit(play_button, play_button_rect)
         screen.blit(banner, banner_rect)
 
     # update game screen
